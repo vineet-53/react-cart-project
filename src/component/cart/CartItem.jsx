38b60b3d82c45d29cee1aa8af5c5de7b594debcb
vineet53 <vineetsinghnegi53@gmail.com>
2023-07-18 08:15:00 +0530

    added cart styling and transition

--- src/component/cart/CartItem.jsx
+++ src/component/cart/CartItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./cart.scss";
 class CartItem extends React.Component {
   constructor(props) {
     const { id, price, desc, title, qty, imgUrl } = props.item;
@@ -15,13 +16,13 @@ class CartItem extends React.Component {
   render() {
     return (
       <div className="cart-item" id={this.state.id}>
-        <LeftCart imgUrl = {this.state.imgUrl} />
+        <LeftCart imgUrl={this.state.imgUrl} />
         <RightCart details={this.state} />
       </div>
     );
   }
 }
-function LeftCart({imgUrl}) {
+function LeftCart({ imgUrl }) {
   return (
     <div className="left-item">
       <img src={imgUrl} alt="phone photo" />
@@ -32,10 +33,15 @@ function RightCart(props) {
   const { title, desc, price, qty } = props.details;
   return (
     <div className="right-item">
-      <h1 className="title">{title}</h1>
+      <h2 className="title">{title}</h2>
       <div className="desc">{desc}</div>
       <div className="price">{price}</div>
       <div className="qty">{qty}</div>
+      <div className="action-buttons">
+        <button className="action-button" id="inc">+</button>
+        <button className="action-button" id="dec">-</button>
+        <button className="action-button" id="delete">delte</button>
+      </div>
     </div>
   );
 }
